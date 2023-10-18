f12a2f26a5319c8f2150a228f99f97ab07519749
AntiToni <s3895923@student.rmit.edu.au>
2023-10-19 00:06:48 +1100

    Changed algorithm to work with vectors

--- algorithm.py
+++ algorithm.py
@@ -18,38 +18,51 @@ def map_to_dir(x,y,z,x_size,z_size):
     z += 1
 
     y_even = y % 2 == 0
-    z_even = z % 2 == 0
+    z_even = z % 2 != 0 if y_even else z % 2 == 0
     z_size_even = z_size % 2 == 0
 
-    dir = '^' if not y_even else 'v'
 
-    # Reverse behaviour if z 
-    if y_even:
-        if z_even:
-            dir = '^'
-            if x == x_size:
-                dir = '<'
-        elif x == 1:
-            dir = '<'
-    else:
-        if z_even:
-            dir = 'v'
-            if x == 1:
-                dir = '>'
-        elif x == x_size:
-            dir = '>'
+    dir = {'x': 1, 'y': 0, 'z': 0}
+
+    # Reverse direction every second column, also add sideways moves
+    if z_even:
+        dir = {'x': -1, 'y': 0, 'z': 0}
+        if x == 1:
+            dir = {'x': 0, 'y': 0, 'z': 1}
+    elif x == x_size:
+        dir = {'x': 0, 'y': 0, 'z': 1}
 
     # End tile check
     if y_even:
+        # Flip direction of arrow if even y
+        dir['z'] *= -1
         if x == 1 and z == 1:
-            dir = 'O'
-    elif z == z_size:
-        if z_size_even and x == 1:
-            dir = 'O'
-        elif not z_size_even and x == x_size:
-            dir = 'O'
+            dir = {'x': 0, 'y': -1, 'z': 0}
+    elif z == z_size and (z_size_even and x == 1 or not z_size_even and x == x_size):
+            dir = {'x': 0, 'y': -1, 'z': 0}
 
     return dir
+
+def grid_to_string(grid):
+    for sublist in grid:
+        for i in range(len(sublist)):
+            string = ''
+            print(sublist, i)
+            if sublist[i] == '':
+                string = ''
+            elif sublist[i]['x'] == 1:
+                string = '^'
+            elif sublist[i]['x'] == -1:
+                string = 'v'
+            elif sublist[i]['y'] == 1:
+                string = 'O'
+            elif sublist[i]['y'] == -1:
+                string = 'X'
+            elif sublist[i]['z'] == 1:
+                string = '>'
+            elif sublist[i]['z'] == -1:
+                string = '<'
+            sublist[i] = string
 
 def print_horizontal_line():
     print('+', end='')
@@ -57,7 +70,7 @@ def print_horizontal_line():
         print('-' * tile_width, end='+')
     print()
 
-def print_grid():
+def print_grid(grid):
     print_horizontal_line()
     for i in range(len(grid) - 1, -1, -1):
         for item in grid[i]:
@@ -68,7 +81,7 @@ def print_grid():
 
 
 if __name__ == '__main__':
-    y = -2
+    y = -3
 
     x_size = 5
     z_size = 6
@@ -76,5 +89,11 @@ if __name__ == '__main__':
     for i in range(x_size):
         for j in range(z_size):
             grid[i][j] = map_to_dir(i,y,j,x_size,z_size)
+
+    print(grid)
+
+    grid_to_string(grid)
+
+    print_grid(grid)
             
-    print_grid()
+    # print_grid(grid_to_string(grid))
