285efcfd62701938c4b8a487e9a6bb11907e14af
AntiToni <s3895923@student.rmit.edu.au>
2023-10-22 23:03:14 +1100

    Added map and updated lua turn to dir (Dot products are not correct dummy)

--- algorithm.py
+++ algorithm.py
@@ -12,12 +12,19 @@ for x in range(15):
         row.append('')
     grid.append(row)
 
-def map_to_dir(x,y,z,x_size,z_size):
+def map_to_dir(x,y,z,x_size,z_size,eff_mode):
     x += 1
     z += 1
+    layer = y
 
-    y_even = y % 2 == 0
-    z_even = z % 2 != 0 if y_even else z % 2 == 0
+    if eff_mode:
+        layer = (y - 1) / 3
+
+        # If not a main level, move down to a valid main level
+        if (y - 1) % 3 != 0: return {'x': 0, 'y': -1, 'z': 0}
+
+    layer_even = layer % 2 == 0
+    z_even = z % 2 != 0 if layer_even else z % 2 == 0
     z_size_even = z_size % 2 == 0
 
 
@@ -32,7 +39,7 @@ def map_to_dir(x,y,z,x_size,z_size):
         dir = {'x': 0, 'y': 0, 'z': 1}
 
     # End tile check
-    if y_even:
+    if layer_even:
         # Flip direction of arrow if even y
         dir['z'] *= -1
         if x == 1 and z == 1:
@@ -80,14 +87,15 @@ def print_grid(grid):
 
 
 if __name__ == '__main__':
-    y = -3
+    y = -5
+    eff_mode = True
 
     x_size = 5
     z_size = 6
 
     for i in range(x_size):
         for j in range(z_size):
-            grid[i][j] = map_to_dir(i,y,j,x_size,z_size)
+            grid[i][j] = map_to_dir(i,y,j,x_size,z_size,eff_mode)
 
     grid_to_string(grid)
 
--- quarry.lua
+++ quarry.lua
@@ -31,11 +31,17 @@ if #args ~= 2 then
     -- Read coordinates
     origin_coord = read_vector(log_file)
     shaft_coord = read_vector(log_file)
+    progress_coord = read_vector(log_file)
     curr_coord = read_vector(log_file)
 
+    -- Load booleans
+    stringtoboolean={ ["true"]=true, ["false"]=false }
+    eff_mode = stringtoboolean[log_file.readLine()]
+    hit_bedrock = stringtoboolean[log_file.readLine()]
+
     -- Read current direction
-    local x_dir = log_file.readLine()
-    local z_dir = log_file.readLine()
+    local x_dir = tonumber(log_file.readLine())
+    local z_dir = tonumber(log_file.readLine())
     curr_dir = vector.new(x_dir,0,z_dir)
   else
     error('Incorrect number of arguments.', 0)
@@ -45,19 +51,23 @@ else
   x_size = tonumber(args[1])
   z_size = tonumber(args[2])
 
-  -- Origin is starting position, shaft is corner of it's shaft, curr is current position
-  origin_coord = vector.new(0,0,0)
-  shaft_coord = vector.new(0,0,0)
-  curr_coord = vector.new(0,0,0)
+  -- Important global coords
+  origin_coord = vector.new(0,0,0)        -- Start position of turtle
+  shaft_coord = vector.new(0,0,0)         -- Coords of corner of turtle's shaft
+  progress_coord = vector.new(0,0,0)      -- Max progress turtle has made in shaft
+  curr_coord = vector.new(0,0,0)          -- Current location of turtle
+
+  eff_mode = false          -- If quarry should be in efficient mode (less movement and fuel used)
+  hit_bedrock = false      -- If the turtle has finished it's mining operation
 
   -- Direction it starts facing will be treated as +x
   curr_dir = vector.new(1,0,0)
 end
 
 function read_vector(handle)
-  local x = handle.readLine()
-  local y = handle.readLine()
-  local z = handle.readLine()
+  local x = tonumber(handle.readLine())
+  local y = tonumber(handle.readLine())
+  local z = tonumber(handle.readLine())
   return vector.new(x,y,z)
 end
 
@@ -65,7 +75,7 @@ function move_and_mine(move_dir)
   local has_block = true
   local data = nil
 
-  if move_dir == 'U' then
+  if move_dir == vector.new(0,1,0) then
     -- Check block isn't in blacklist, then try mining and check if it worked
     repeat
       has_block, data = turtle.inspectUp()
@@ -79,8 +89,8 @@ function move_and_mine(move_dir)
       -- Handle events before sleeping
       sleep(0)
     until not turtle.detectUp()
-    turtle.up()
-  elseif move_dir == 'D' then
+    move_up()
+  elseif move_dir == vector.new(0,-1,0) then
     -- Check block isn't in blacklist, then try mining and check if it worked
     repeat
       has_block, data = turtle.inspectDown()
@@ -94,9 +104,9 @@ function move_and_mine(move_dir)
       -- Handle events before sleeping
       sleep(0)
     until not turtle.detectDown()
-    turtle.down()
+    move_down()
   else
-    turn_to_dir(DIR[move_dir])
+    turn_to_dir(move_dir)
     
     -- Check block isn't in blacklist, then try mining and check if it worked
     repeat
@@ -111,7 +121,7 @@ function move_and_mine(move_dir)
       -- Handle events before sleeping
       sleep(0)
     until not turtle.detect()
-    turtle.forward()
+    move_forward()
   end
 
   return true
@@ -119,21 +129,29 @@ end
 
 function turn_to_dir(target_dir)
   local turn = 0
-  local product = curr_dir:dot(target_dir)
+  local product = vector.new(curr_dir.x * target_dir.x, curr_dir.y * target_dir.y, curr_dir.z * target_dir.z)
+  print('CURR')
+  print(curr_dir)
+  print('TARGET')
+  print(target_dir)
+  print('DOT')
+  print(product)
 
   -- Check if in correct rotation already
-  if curr_dir.x ~= target_dir.x or curr_dir.z ~= target_dir.z then
+  if (curr_dir.x ~= target_dir.x) or (curr_dir.z ~= target_dir.z) then
     if product ~= vector.new(0,0,0) then
       -- Need to turn 180
       turn = 2
     else
       -- Must turn left or right
       local swap = vector.new(target_dir.z, target_dir.y, target_dir.x)
-      local swap_product = curr_dir:dot(swap)
+      local swap_product = vector.new(curr_dir.x * swap.x, curr_dir.y * swap.y, curr_dir.z * swap.z)
 
       turn = swap_product.x - swap_product.z
     end
   end
+
+  print(turn)
 
   -- Make turns
   while turn > 0 do
@@ -149,17 +167,68 @@ function turn_to_dir(target_dir)
   curr_dir = vector.new(target_dir.x, 0, target_dir.z)
 end
 
-function map_to_dir(x, y, z, x_size, z_size)
-  return nil
+function move_forward()
+  curr_coord = curr_coord + curr_dir
+  turtle.forward()
+end
+
+function move_up()
+  curr_coord = curr_coord + DIR['U']
+  turtle.up()
+end
+
+function move_down()
+  curr_coord = curr_coord + DIR['D']
+  turtle.down()
+end
+
+function map_to_dir(coord, x_size, z_size)
+  local x = coord.x + 1
+  local z = coord.z + 1
+  local layer = coord.y
+
+  if eff_mode then
+    layer = (coord.y - 1) / 3
+    -- If not a main level, move down to a valid main level
+    if (coord.y - 1) % 3 ~= 0 then return vector.new(0,-1,0) end
+  end
+
+  local layer_even = layer % 2 == 0
+  local z_even
+  if layer_even then
+    z_even = z % 2 ~= 0
+  else
+    z_even = z % 2 == 0
+  end
+  local z_size_even = z_size % 2 == 0
+
+  local dir = vector.new(1,0,0)
+
+  -- Reverse direction every second column, also add sideways moves
+  if z_even then
+    dir = vector.new(-1,0,0)
+    if x == 1 then
+      dir = vector.new(0,0,1)
+    end
+  elseif x == x_size then
+    dir = vector.new(0,0,1)
+  end
+
+  -- End tile check
+  if layer_even then
+      -- Flip direction of arrow if even y
+      dir.z = dir.z * -1
+      if x == 1 and z == 1 then
+          dir = vector.new(0,-1,0)
+      end
+  elseif z == z_size and ((z_size_even and x == 1) or (not z_size_even and x == x_size)) then
+    dir = vector.new(0,-1,0)
+  end
+
+  return dir
 end
 
 -- Main Program
 for i = 1, 10, 1 do
-  move_and_mine('E')
+  move_and_mine(map_to_dir(curr_coord,x_size,z_size))
 end
- 
-for i = 1, 10, 1 do
-  move_and_mine('W')
-end
- 
-turn_to_dir(DIR['E'])
